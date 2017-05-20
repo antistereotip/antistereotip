e4bf68b6c3d17cfbe8f15664f6566ba8a308f3b0
Milutin Gavrilovic <milutingavrilovic@yahoo.com>
2017-05-20 14:43:47 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Antistereotip</title>
+  <meta name="description" content="Nije znanje znanje znati, vec je znanje znanje dati">
+  <meta name="author" content="hightech">
+
+  <link rel="stylesheet" href="style.css">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+
+<nav>
+    <ul>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">O nama</a></li>
+        <li><a href="#">Arhiv</a></li>
+        <li><a href="#">Kontakt</a></li>
+    </ul>
+</nav>
+
+<marquee>Antistereotip Coming Soon</marquee>
+
+<section class="left ex8">
+<h1>Naslov</h1>
+<p>Paragraf</p>
+<img src="media/anon.jpg" width="100%" height="auto" alt="anon">
+</section>
+
+<section class="right ex8">
+<h1>Desni Naslov</h1>
+<p>Desni Paragraf</p>
+
+</section>
+
+<aside class="ex8">
+<h1>Desni sidebar</h1>
+<p>Sidebar Paragraf</p>
+</aside>
+
+  <script src="js/scripts.js"></script>
+</body>
+</html>
