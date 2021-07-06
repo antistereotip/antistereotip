da487ee13414ba91738895cfea638e3e9a801fc0
Milutin Gavrilovic <milutingavrilovic@yahoo.com>
2021-07-06 19:33:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,18 @@
-<!doctype html>
-
-<html lang="en">
+<html>
 <head>
-  <meta charset="utf-8">
-
-  <title>Antistereotip</title>
-  <meta name="description" content="Nije znanje znanje znati, vec je znanje znanje dati">
-  <meta name="author" content="hightech">
-
-  <link rel="stylesheet" href="style.css">
-
-  <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-  <![endif]-->
+<style>
+  .center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 100px;
+  width: 50%;
+}
+  h1 {color: white;text-align:center;}
+</style>
 </head>
-
-<body>
-
-<nav>
-    <ul>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">O nama</a></li>
-        <li><a href="#">Arhiv</a></li>
-        <li><a href="#">Kontakt</a></li>
-    </ul>
-</nav>
-
-<marquee>Antistereotip Coming Soon</marquee>
-
-<section class="left ex8">
-<h1>Naslov</h1>
-<p>Paragraf</p>
-<img src="anon.jpg" width="100%" height="auto" alt="anon">
-</section>
-
-<section class="right ex8">
-<h1>Desni Naslov</h1>
-<p>Desni Paragraf</p>
-
-</section>
-
-<aside class="ex8">
-<h1>Desni sidebar</h1>
-<p>Sidebar Paragraf</p>
-</aside>
-
-  <script src="js/scripts.js"></script>
+<body style="background: #0e0e0e;">
+  <h1>Ghost Protocol</h1>
+<img style="-webkit-user-select: none;" src="supernet.gif" width="500" class="center">
 </body>
 </html>
