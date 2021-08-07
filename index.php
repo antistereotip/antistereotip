2851fe798b35d0bbc12fb957af968176493dfe78
Milutin Gavrilovic <milutingavrilovic@yahoo.com>
2021-08-07 19:48:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<html>
-<head>
-<style>
-  .center {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 100px;
-  width: 50%;
-}
-  h1 {color: white;text-align:center;}
-</style>
-</head>
-<body style="background: #0e0e0e;">
-  <h1>Ghost Protocol - Antistereotip</h1>
-<img style="-webkit-user-select: none;" src="supernet.gif" width="500" class="center">
-</body>
-</html>
+<?php
+header ('Location: code.html');
+?>
