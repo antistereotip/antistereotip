4d7fc94d9ef2ca4f7827bd83ab650a892880dd4d
Milutin Gavrilovic <milutingavrilovic@yahoo.com>
2021-08-27 23:08:38 +0200

    Update top-cache.php

--- cache/top-cache.php
+++ cache/top-cache.php
@@ -5,11 +5,11 @@ $file = $break[count($break) - 1];
 $cachefile = 'cached-'.substr_replace($file ,"",-4).'.html';
 $cachetime = 18000;
 
-// Serve from the cache if it is younger than $cachetime
+// Serviraj iz kesa ako je mladje od $cachetime
 if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile)) {
     echo "<!-- Cached copy, generated ".date('H:i', filemtime($cachefile))." -->\n";
     readfile($cachefile);
     exit;
 }
-ob_start(); // Start the output buffer
+ob_start(); // Startuj output buffer
 ?>
