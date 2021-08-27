ef80f53cd94c9fef249fd8156e5449ab1cce52c8
antistereotip <milutingavrilovic@yahoo.com>
2021-08-27 23:14:47 +0200

    cache second and third page added

---
+++ cache/second.php
@@ -0,0 +1,9 @@
+<?php
+
+include('top-cache.php'); 
+
+echo "<p>Druga stranica u cache sistemu</p>";
+
+include('bottom-cache.php');
+?>
+
---
+++ cache/third.php
@@ -0,0 +1,9 @@
+<?php
+
+include('top-cache.php'); 
+
+echo "<p>Treca stranica u cache sistemu</p>";
+
+include('bottom-cache.php');
+?>
+
