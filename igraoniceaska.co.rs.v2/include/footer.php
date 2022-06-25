5a713d989d36d82144132669d689fb677acbff9a
antistereotip <milutingavrilovic@yahoo.com>
2022-06-25 14:06:49 +0200

    Igraonice ASKA

---
+++ igraoniceaska.co.rs.v2/include/footer.php
@@ -0,0 +1,116 @@
+   <div class="my-3 p-3 bg-body rounded shadow-sm sedamdeset animacija" id="video">
+	<h2>Lasermaze Aska</h2><br />
+	<iframe width="100%" height="450" src="https://www.youtube.com/embed/MVuU1r-JM_E" title="YouTube video player" 
+	frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+	allowfullscreen></iframe>
+   </div>
+   
+   <div class="my-3 p-3 bg-body rounded shadow-sm sedamdeset animacija" id="odgovornost">
+	<h2>Odricanje od odgovornosti</h2><br />
+	<p>Odričemo se odgovornosti ukoliko....</p>
+   </div>
+   
+   <div class="my-3 p-3 bg-body rounded shadow-sm sedamdeset animacija" id="politika-privatnosti">
+	<h2>Politika privatnosti</h2><br />
+	<p>Email adrese skupljamo isključivo radi promocije naših novih i postojećih usluga i proizvoda.</p>
+   </div>
+   
+   <div class="my-3 p-3 bg-body rounded shadow-sm sedamdeset animacija" id="licence">
+	<h2>Licence i sertifikati</h2><br />
+	<p>Na sve naše proizvode dajemo garanciju godinu dana i prateću dokumentaciju koja sadrži sertifikate EN 1176. Iz tog razloga ne upotrebljavamo 
+	lošije materijale kao npr shoteks, pvc ili igličasto pleten kanap. Naši kanapi kao što smo rekli su atestirani i izdržljivi. Kanap za mreže je
+	atestiran na 107 kp prekidnog momenta. Licencirani smo po poslednjim standardima.</p>
+   </div>
+   
+   <div class="my-3 p-3 bg-body rounded shadow-sm sedamdeset animacija" id="kontakt">
+	<h2>Kontaktiraj nas</h2><br />
+	<p>PIB: 109792022</p>
+	<p>eMail:<br />
+	aska.szr@gmail.com<br />
+	igraoniceaska@gmail.com</p>
+	<p>Telefon: +381643939674</p>
+    <p>Linkovi:<br />
+        <a href="https://opremazaigraonice.rs" target="__blank" rel="noopener noreferrer">WWW.OpremaZaIgraonice.RS</a><br />
+        <a href="https://igraoniceaska.co.rs" target="__blank" rel="noopener noreferrer">WWW.IgraoniceAska.CO.RS</a><br />
+        <a href="https://opremanje-decijih-igraonica.com" target="__blank" rel="noopener noreferrer">WWW.Opremanje-Decijih-Igraonica.COM</a><br />
+        <a href="https://laser-pinkpanter-training-center.com" target="__blank" rel="noopener noreferrer">WWW.Laser-Pinkpanter-Training-Center.COM</a><br />
+        <a href="https://indoor-playgrounds.biz/" target="__blank" rel="noopener noreferrer">WWW.Indoor-Playgrounds.BIZ</a><br />
+        <a href="https://opremazaigraonice.rs/video.html" target="__blank" rel="noopener noreferrer">Video Materials</a><br />
+        <a href="https://igraoniceaska.github.io/" target="__blank" rel="noopener noreferrer">Github IO</a>
+    </p>
+    <p>Društvene mreže</p>
+    <p>
+    <a href="https://www.instagram.com/igraoniceaska/" target="__blank" rel="noopener noreferrer"><img src="./media/Instagram-Icon.png" width="42" height="42" alt="instagram"></a>
+    <a href="https://linkedin.com/in/aska-playgrounds-8b217269" target="__blank" rel="noopener noreferrer"><img src="./media/Linkedin-Icon.png" width="42" height="42" alt="Linkedin"></a>
+    <a href="https://www.facebook.com/OpremaZaDecijeIgraonice" target="__blank" rel="noopener noreferrer"><img src="./media/Facebook-Icon.png" width="42" height="42" alt="Facebook"></a>
+    </p>
+  
+
+
+	
+	
+	</div>
+	
+	<div class="my-3 p-3 bg-body rounded shadow-sm sedamdeset animacija" style="font-weight: bold; font-size: 0.8em;" id="tag-cloud">
+	
+	<p style="margin-top: 7%; color: #555; font-size:1.2em;"><b>Oblak oznaka</b></p>
+    <p>
+<span style="color:blue;">Igraonice ASKA</span>,
+<span style="color:#008B8B;font-size:1.2em;"> Izrada igraonica</span>, 
+<span style="color:#FF7F50; font-size:1.2em"> Oprema za igraonice</span>, 
+<span style="color:green; font-size:1.2em;"> Bajkovit dizajn</span>,
+<span style="color:red; font-size:1.1em"> Sigurne i atestirane konstrukcije</span>,
+<span style="color:#FF7F50;"> Trampolina parkovi</span>, 
+<span style="color: green;"> Tobogani</span>, 
+<span style="color:#FF7F50; font-size:1.3em"> Tende</span>, 
+<span style="color:#FF7F50;"> Najkvalitetniji materijal</span>,  
+<span style="color:red; font-size:1.1em"> Sigurnosni sertifikati</span>,
+<span style="color:#FF7F50; font-size:1.3em"> Zabava</span>,
+<span style="color:violet; font-size:1.4em;"> Laser maze</span>,
+<span style="color:lightgreen; font-size:1.1em;"> Laser tag</span>, 
+<span style="color:orangered; font-size:1.2em;"> Trampolina parkovi</span>, 
+<span style="color:#7FFFD4; font-size:1.2em"> Tržni centri</span>, 
+<span style="color:#008B8B;font-size:1.2em;"> 3D Elementi</span>, 
+<span style="color:#7FFFD4; font-size:1.2em"> Grafika, Oblici </span>, 
+<span style="color:green; font-size:1.1em;"> Osvetljenje</span>, 
+<span style="color:blue; font-size:1.1em;"> Soft Play</span>,
+<span style="color:#7FFFD4;"> Oslikavanje zidova</span>,
+<span style="color:#7FFFff; font-size:1.3em;"> Izrada igraonica</span>,
+<span style="color:#7FFFD4; font-size: 1.2em;"> Jumping arene</span>,
+<span style="color:#17FeD4; font-size:1.5em"> Trampoline sistemi</span>,
+<span style="color:orangered; font-size1.3em"> Trampoline sa senzorima</span>,
+<span style="color:green; font-size:1.2em;"> Baby bazeni</span>,
+<span style="color:violet; font-size: 1.4em;"> Decije Igraonice za hotele</span>,
+<span style="color:#7aaFD4;font-size:1.2em;"> Oslikavanje zidova</span>,
+<span style="color:#13f443; font-size:1.5em;"> Vrteske za igraonice</span>,
+<span style="color:orangered; font-size:1.3em;"> Bazeni sa lopticama</span>,
+<span style="color:#7FFFD4; font-size:1.3em;"> Zip Line za igraonice</span>,
+<span style="color:#7FFFD4;font-size:1.6em;"> Lijane na sajli</span>,
+<span style="color:violet;font-size:1.2em;"> Avantura staze za igraonice</span>,
+<span style="color:blue;font-size:1.2em;"> Decije igraonice</span>,
+<span style="color:green;font-size:1.5em;"> Igraonice za decu</span>,
+<span style="color:blue;font-size:1.2em;"> Playground</span>,
+<span style="color:orangered;font-size:1.2em;"> LaserTag arene</span>,
+<span style="color:#7FFFff; font-size:1.3em;"> Mehanika</span>,
+<span style="color:#7FFFD4; font-size: 1.2em;"> Fizika</span>,
+<span style="color:#17FeD4; font-size:1.5em"> 3D Štampa</span>,
+<span style="color:orangered; font-size1.3em"> Makete</span>,
+<span style="color:orangered; font-size:1.2em;"> Izrada maketa</span>,
+<span style="color:violet; font-size: 1.4em;"> Autocad</span>,
+<span style="color:#eeeeD4; font-size:1.5em;"> 3Ds Max</span>,
+<span style="color:orangered; font-size:1.3em;"> Avantura parkovi</span>,
+<span style="color:#7FFFD4; font-size:1.3em;"> Prepreke</span>,
+<span style="color:#7FFFD4;font-size:1.6em;"> Prototip</span>,
+
+
+</p>
+	</div>
+   
+   <div class="my-3 p-3 bg-body rounded shadow-sm sedamdeset animacija">
+    <footer id="footer" >
+     <p>Powered by <a href="https://antistereotip.net">@hightech</a> & Protected by <a href="https://usk.rs">@uskrs</a> &#8620;</p>
+    </footer>
+   </div>
+   
+
+   
---
+++ igraoniceaska.co.rs.v2/include/head.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta name="keywords" content="unutrasnja igralista, igraonice, oprema za igraonice, laser tag, laser maze, painting zidovi, trampoline, jumping arene, spoljnja igralista, opremanje igraonica, tobogani...">
+    <meta name="description" content="Aska prostori za igru i oprema je napravljena sa ljubavlju i maštom. Posebno pažnja je posvećena kvalitetu i bezbednosti. Svi proizvodi i materijali su sertifikati po najvišim standardima.">
+    <meta name="author" content="Milutin Gavrilovic">
+    <meta name="generator" content="hightech 007">
+    <title>Aska</title>
+    <meta name="google-site-verification" content="sao-GkpCGrokKc2HF-jvsfiKbbD_3U2QEkOIpaFGnjc" />
+	<meta property="og:title" content="Aska &amp; Igraonice" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="./media/aska.png" />
+    <meta property="og:url" content="https://igraoniceaska.co.rs/" />
+    <meta property="og:description" content="Aska prostori za igru i oprema je napravljena sa ljubavlju i maštom. Posebno pažnja je posvećena kvalitetu i bezbednosti. Svi proizvodi i materijali su sertifikati po najvišim standardima." />
+	<link rel="icon" type="image/png" href="./media/aska.png" />
+    <link rel="canonical" href="https://igraoniceaska.co.rs/">
+
+    <meta name="robots" content="index, follow" />
+    <!-- Bootstrap core CSS -->
+	<link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">
+	    
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LH62YTQ1D5"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LH62YTQ1D5');
+    </script>
+    
+
+    <style>
+	  @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;611&family=Raleway:wght@200&family=Ubuntu:wght@300&display=swap');
+	 
+	  body {font-family: 'Oswald', sans-serif;
+      font-family: 'Raleway', sans-serif;
+      font-family: 'Ubuntu', sans-serif;}
+	  .btn {background: #f5785b; border-radius: 0px !important;}
+	  .form-control, me-2 {border-radius: 0px !important;}
+	  .btn-outline-success {color: white !important;}
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+	  footer p {text-align: center;}
+	  .animacija {
+        -webkit-animation: cssAnimation 2s 1 ease;
+        -moz-animation: cssAnimation 2s 1 ease;
+        -o-animation: cssAnimation 2s 1 ease;
+        }
+        @-webkit-keyframes cssAnimation {
+        from { -webkit-transform: rotate(0deg) scale(0.100) skew(0deg) translate(0px); }
+        to { -webkit-transform: rotate(0deg) scale(0.999) skew(0deg) translate(0px); }
+        }
+        @-moz-keyframes cssAnimation {
+        from { -moz-transform: rotate(0deg) scale(0.100) skew(0deg) translate(0px); }
+        to { -moz-transform: rotate(0deg) scale(0.999) skew(0deg) translate(0px); }
+        }
+        @-o-keyframes cssAnimation {
+        from { -o-transform: rotate(0deg) scale(0.100) skew(0deg) translate(0px); }
+        to { -o-transform: rotate(0deg) scale(0.999) skew(0deg) translate(0px); }
+        
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="./assets/offcanvas.css" rel="stylesheet">
+    <link href="./assets/style.css" rel="stylesheet">
+    <link rel="stylesheet" media="screen and (max-width: 600px)" href="assets/mobilni.css">
+  </head>
+  
+  <body class="bg-light">
