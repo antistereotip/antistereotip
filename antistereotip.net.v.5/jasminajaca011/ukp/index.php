d2dad39bca54d3b06f012a36e66a4ef280d87143
antistereotip <milutingavrilovic@yahoo.com>
2022-06-25 14:24:41 +0200

    v5 update

---
+++ antistereotip.net.v.5/jasminajaca011/ukp/index.php
@@ -0,0 +1,5 @@
+<form action="myprocessingscript.php" method="POST">
+    <input name="username" type="text" placeholder="Username" />
+    <input name="password" type="password" placeholder="Password"/>
+    <input type="submit" name="submit" value="Submit">
+</form>
