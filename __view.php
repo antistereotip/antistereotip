c3ef432c61245fd854a38bf5141810f5c7a58b47
Milutin Gavrilović <milutingavrilovic@yahoo.com>
2022-07-21 00:29:02 +0200

    Create __view.php

---
+++ __view.php
@@ -0,0 +1,9 @@
+<?php
+require_once('db.php');
+$pdo_statement = $pdo_conn->prepare("SELECT * FROM posts where id=" . $_GET["id"]);
+$pdo_statement->execute();
+$result = $pdo_statement->fetchAll();
+?>
+<b>Naslov:</b> <?php echo $result[0]['post_title']; ?>
+<b>Ukratko:</b> <?php echo $result[0]['sum']; ?>
+<b>Opis:</b> <?php echo $result[0]['description']; ?>
