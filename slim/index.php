7e972216be41d25086d2f6d45b04c189065dcac2
Milutin Gavrilović <milutingavrilovic@yahoo.com>
2024-06-05 23:30:31 +0200

    zastave slim

---
+++ slim/index.php
@@ -0,0 +1,25 @@
+<?php 
+header('Location: https://antistereotip.net/slim/razumni-rs.html');
+require_once 'inc/funkcije.php'; 
+$home='current'; 
+$second='normall'; 
+$contact='normall'; 
+require_once 'inc/misc/header.php'; 
+
+org("org", "antistereotip", "AntiStereotip | presents", "media/png/engine-up.png",
+"Naš je cilj da srušimo granice siromaštva, jer znanje nema granice. Znanje ljude čini boljima. 
+Radimo da najsiromašnije slojeve društva pripremimo za najtraženije veštine.
+", "Url", "https://antistereotip.github.io", "forum", "#", "RetroPort", 
+"https://antistereotip.github.io/ng1np.html", "h3dreklame");
+
+org("org", "webserveri", "WebServeri | org", "media/png/wlogo.png", "WebServeri(WS1) je projekat otvorenog koda, 
+baziran na efikasnoj podršci, vanstandardnom dizajnu, 
+brzini, sigurnosti... Zajednica predstavlja skup ljudi koji entuzijazmom i radom promovišu otvoreni 
+kod u domenu zajedničkog znanja i interesovanja.", "Url", "https://ws1.github.io", "forum", "#", 
+"RetroPort", "https://antistereotip.github.io", "h3dreklame"); 
+
+
+
+require_once 'inc/misc/footer.php'; 
+?>
+
