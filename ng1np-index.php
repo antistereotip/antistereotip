3a9e4e98427430c4b47be704f780045af46731f5
Milutin Gavrilović <milutingavrilovic@yahoo.com>
2026-05-01 17:31:53 +0200

    Add ng1np-index.php for routing and page rendering

---
+++ ng1np-index.php
@@ -0,0 +1,39 @@
+<?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE); 
+//error_reporting(0);
+#--------engine up---------------------------------------#
+#--------hightech - It's not a bug, it's a feature-------#
+
+#------------------definisi root putanju-----------------#
+define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
+#-----------------pozovi klasu za renderovanje-----------#
+require 'set/class.php';
+
+#---------------------------ruter------------------------#
+$route = isset($_GET['route']) ? $_GET['route'] : '';
+
+
+#-------------------dozvoljene_stranice------------------#
+$pages = array (
+'default' => './lib/default.php',
+'second' => './lib/second.php',
+'third' => './lib/third.php',
+'fourth' => './lib/fourth.php',
+'fifth' => './lib/fifth.php',
+'god' => './lib/god.php',
+'user' => './lib/default.php',
+'root' => './lib/default.php',
+'sleep' => './lib/default.php',
+'aktivan' => './lib/default.php',
+'neaktivan' => './lib/default.php'
+);
+
+#------proverava da li je string u nizu veci od nule i da li je u nizu------#
+if( (strlen($pages[$route]) > 0) && (in_array($pages[$route], $pages)) ){
+
+	#-------pozovi stranicu po ruti--------#
+	require $pages[$route];
+
+#-----u suprotnom poziva default stranicu-----#
+} else { require_once(ROOT_DIR .'lib/default.php'); }
+?>
