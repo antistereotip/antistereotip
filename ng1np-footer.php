417f8959e79e086446f1739e250bc0c1dc4d91a9
Milutin Gavrilović <milutingavrilovic@yahoo.com>
2026-05-01 17:36:35 +0200

    Add footer to ng1np-footer.php

---
+++ ng1np-footer.php
@@ -0,0 +1,7 @@
+<footer>
+<p style="color: #fff;">Ng1np  &#169;  2026</p><p></p>
+<p style="color: orange; font-size: 0.8em;">Powered  By  Uprava & Protected by hightech </p>
+
+</footer>
+</body>
+</html>
