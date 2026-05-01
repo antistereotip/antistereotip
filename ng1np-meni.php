743d24982f50fe0a2cd4817f44391036e81a7233
Milutin Gavrilović <milutingavrilovic@yahoo.com>
2026-05-01 17:35:28 +0200

    Add navigation and Fibonacci functions to ng1np-meni.php

---
+++ ng1np-meni.php
@@ -0,0 +1,28 @@
+<nav class="animacija">
+<li><a href="?route=default">[  Default  ]</a></li>
+<li><a href="?route=second">[  Second  ]</a></li>
+<li><a href="?route=third">[  Third  ]</a></li>
+<li><a href="?route=fourth">[  Fourth  ]</a></li>
+<li><a href="?route=fifth">[  Fifth  ]</a></li>
+<li><a href="https://usk.rs/btc/javnikljuc.txt">[ PGP Public Key ]</a></li>
+<li><a href="https://usk.rs/dinovzabavnik32bita.pdf">[ Dinov Zabavnik ]</a></li>
+<li><a href="https://usk.rs/Antistereotip-Plan-B.pdf">[ Plan B ]</a></li>
+<li><a href="https://usk.rs/scripts/salata.php">[ Salata ]</a></li>
+<li><a href="https://usk.rs/scripts/rostilj-kalkulator.php">[ Rostilj ]</a>
+</nav>
+<p></p>
+<p>&#8594; <?php sitemapBrojacStranica(); ?></p>
+<p>
+<?php
+$number = 20;
+echo "&#8594; Fibonacci (20 terms): ";
+for ($counter = 0; $counter < $number; $counter++){  
+    echo Fibonacci($counter),' ';
+}
+?>
+</p>
+<p>&#8594; Reverse Fibonacci (20 terms): <?php $n = 20; reverseFibonacci($n); ?></p>
+
+
+
+
